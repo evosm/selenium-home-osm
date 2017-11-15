5425768966afd6cdd55d870d2d0e7243a69fcdf2
evosm <ev.osmakov@yandex.ru>
2017-11-15 13:48:24 +0300

    updated

--- sticker_app.py
+++ sticker_app.py
@@ -3,7 +3,7 @@ import unittest, time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-class test_sticker(unittest.TestCase):
+class test_two(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -12,14 +12,17 @@ class test_sticker(unittest.TestCase):
 
     def test_login(self):
         driver = self.driver
+        driver.implicitly_wait(5)
         try:
             driver.get(self.base_url)
-            driver.implicitly_wait(5)
             product_list = driver.find_elements_by_css_selector('li.product')
-            for id in range(len(product_list)):
-                sticker = product_list[id].find_elements_by_css_selector("div.sticker")
-                if len(sticker) == 0 or len(sticker) > 1:
-                    self.fail('error')
+            if len(product_list) == 0:
+                self.fail(r'no elements')
+            else:
+                for id in range(len(product_list)):
+                    sticker = product_list[id].find_elements_by_css_selector("div.sticker")
+                    if len(sticker) == 0 or len(sticker) > 1:
+                        self.fail('error')
         except NoSuchElementException as e:
             print('error:', e)
             self.fail(r'element error')
@@ -29,5 +32,5 @@ class test_sticker(unittest.TestCase):
 
 if __name__ == "__main__":
     print('\nНачинаем тест\n')
-    suite = unittest.TestLoader().loadTestsFromTestCase(test_sticker)
+    suite = unittest.TestLoader().loadTestsFromTestCase(test_two)
     unittest.TextTestRunner(verbosity=2).run(suite)
