6b90eef5f99b0f471a08b3bc53715e5dc4c61874
evosm <ev.osmakov@yandex.ru>
2017-11-28 18:03:59 +0300

    удалил time.sleep

--- task_7_adminPanel_click.py
+++ task_7_adminPanel_click.py
@@ -9,24 +9,22 @@ class test_one(unittest.TestCase):
         self.driver = webdriver.Chrome()
         #self.driver = webdriver.Safari()
         self.base_url = "http://localhost/litecart/admin/"
+        driver.implicitly_wait(5)
 
     def test_login(self):
         driver = self.driver
         try:
             driver.get(self.base_url)
-            time.sleep(5)
             user_name_elem = driver.find_element_by_xpath("//*[@type='text']")
             user_name_elem.send_keys('admin')
             user_pass_elem = driver.find_element_by_name('password')
             user_pass_elem.send_keys('admin')
             driver.find_element_by_xpath("//*[@type='submit']").click()
-            time.sleep(3)
             menu = driver.find_element_by_xpath("//*[@class='list-vertical']")
             elements_menu = menu.find_elements_by_tag_name("li")
             for id in range(len(elements_menu)):
                 print(elements_menu[id].text)
                 elements_menu[id].click()
-                time.sleep(3)
                 driver.find_elements_by_tag_name('h1')
                 try:
                     docs = driver.find_element_by_xpath("//*[@class='docs']")
@@ -34,7 +32,6 @@ class test_one(unittest.TestCase):
                     for idx in range(len(list_docs)):
                         print(list_docs[idx].text)
                         list_docs[idx].click()
-                        time.sleep(3)
                         driver.find_elements_by_tag_name('h1')
                         docs = driver.find_element_by_xpath("//*[@class='docs']")
                         list_docs = docs.find_elements_by_tag_name('li')
