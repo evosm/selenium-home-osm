c80829061c2250e5393bd24ad6437e9ce5018b07
evosm <ev.osmakov@yandex.ru>
2017-11-30 12:00:11 +0300

    updated

--- task_19.py
+++ task_19.py
@@ -18,14 +18,11 @@ class two(unittest.TestCase):
         self.checkout_page = checkout_page(base_url=self.base_url,driver=self.driver,wait=self.wait)
 
     def test_19(self):
-        try:
-            self.driver.get(self.base_url)
-            for i in range(0, 3):
-                self.main_page.work_on_page()
-                self.product_page.work_on_page()
-            self.checkout_page.work_on_page()
-        except NoSuchElementException as e:
-            print('error:', e)
+        self.main_page.start()
+        for i in range(0,3):
+            self.main_page.work_on_page()
+            self.product_page.work_on_page()
+        self.checkout_page.work_on_page()
 
     def tearDown(self):
             self.driver.close()
@@ -35,8 +32,16 @@ class main_page:
         self.base_url = kwargs['base_url']
         self.driver = kwargs['driver']
 
+    def start(self):
+        self.driver.get(self.base_url)
+
     def work_on_page(self):
-        self.driver.find_element_by_css_selector('li.product.column.shadow.hover-light').click()
+        try:
+            self.driver.find_element_by_css_selector('li.product.column.shadow.hover-light').click()
+        except NoSuchElementException as e:
+            print('error:', e)
+        except TimeoutError as e:
+            print('error', e)
 
 class product_page:
     def __init__(self,**kwargs):
@@ -49,34 +54,46 @@ class product_page:
             sel.select_by_index(1)
         except NoSuchElementException as e:
             print('no select', e)
-        quantity = self.driver.find_element_by_css_selector('span.quantity').text
-        s = int(quantity)+1
-        self.driver.find_element_by_name('add_cart_product').click()
-        if (self.wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#cart > a.content > span.quantity'),str(s)))):
-            self.driver.find_element_by_css_selector('#breadcrumbs > ul > li:nth-child(1) > a').click()
-            print('added')
-        else:
-            print('adding error')
+        try:
+            quantity = self.driver.find_element_by_css_selector('span.quantity').text
+            s = int(quantity)+1
+            self.driver.find_element_by_name('add_cart_product').click()
+            if (self.wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#cart > a.content > span.quantity'),str(s)))):
+                self.driver.find_element_by_css_selector('#breadcrumbs > ul > li:nth-child(1) > a').click()
+                print('added')
+            else:
+                print('adding error')
+        except NoSuchElementException as e:
+            print('no select', e)
+        except TimeoutError as e:
+            print('error', e)
 
 class checkout_page:
     def __init__(self,**kwargs):
         self.driver = kwargs['driver']
         self.wait = kwargs['wait']
     def work_on_page(self):
-        self.driver.find_element_by_partial_link_text('Checkout »').click()
-        for i in range(0, 3):
-            self.driver.find_element_by_name('remove_cart_item').click()
-            wait_one = self.wait.until(EC.staleness_of(self.driver.find_element_by_css_selector(
-                '#order_confirmation-wrapper > table > tbody > tr.footer > td:nth-child(2) > strong')))
-            if i != 2:
-                wait_two = self.wait.until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, '#order_confirmation-wrapper > table')))
-            if (wait_one and wait_two):
-                print('removed')
-            else:
-                print('remove error')
-        if self.driver.find_element_by_tag_name('em').text == 'There are no items in your cart.':
-            print('Completed')
+        try:
+            self.driver.find_element_by_partial_link_text('Checkout »').click()
+            for i in range(0, 3):
+                elem_table = self.driver.find_element_by_css_selector(
+                    '#order_confirmation-wrapper > table > tbody > tr.footer > td:nth-child(2) > strong')
+                self.driver.find_element_by_name('remove_cart_item').click()
+                wait_one = self.wait.until(EC.staleness_of(elem_table))
+                if i != 2:
+                    wait_two = self.wait.until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, '#order_confirmation-wrapper > table')))
+                if (wait_one and wait_two):
+                    print('removed')
+                else:
+                    print('remove error')
+            if self.driver.find_element_by_tag_name('em').text == 'There are no items in your cart.':
+                print('Completed')
+        except NoSuchElementException as e:
+            print('no select', e)
+        except TimeoutError as e:
+            print('error', e)
+
 
 if __name__ == "__main__":
     print('\nНачинаем тест\n')
